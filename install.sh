e6c85e6788795cc0994ce3f26448500bef95aac0
Dan Williams <dan.williams@solarwinds.com>
2020-04-08 09:56:17 -0400

    Use local go buildchain

--- install.sh
+++ install.sh
@@ -2,25 +2,6 @@
 
 set -e
 
-current_version=$(sed -n -e 's/version:[ "]*\([^"]*\).*/\1/p' $(dirname $0)/plugin.yaml)
-HELM_CONVERT_VERSION=${HELM_CONVERT_VERSION:-$current_version}
-
 dir=${HELM_PLUGIN_DIR:-"$(helm home)/plugins/helm-convert"}
-os=$(uname -s | tr '[:upper:]' '[:lower:]')
-release_file="helm-convert_${os}_${HELM_CONVERT_VERSION}.tar.gz"
-url="https://github.com/deedubs/helm-convert/releases/download/v${HELM_CONVERT_VERSION}/${release_file}"
 
-mkdir -p $dir
-
-if command -v wget
-then
-  wget -O ${dir}/${release_file} $url
-elif command -v curl; then
-  curl -L -o ${dir}/${release_file} $url
-fi
-
-tar xvf ${dir}/${release_file} -C $dir
-
-chmod +x ${dir}/helm-convert
-
-rm ${dir}/${release_file}
+go build -o ${dir}/helm-convert cmd/convert.go
